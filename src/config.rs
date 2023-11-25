e663365b5afecb9fe10b4acc4b2114ab304c9823
IT21803420 <it21803420@my.sliit.lk>
2023-11-25 10:10:50 +0530

    added new dependancy

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,15 @@
+use std::{
+    fs,
+    net::{TcpListener,TcpStream},
+    io::{prelude::*, BufReader},
+    string::ParseError,
+};
 pub struct Config{
-    port: i8, // user-defined port <- used for localhost address (127.0.0.1)
+    port: i32, // user-defined port <- used for localhost address (127.0.0.1)
     url: String // path for the file that is located within the users system
 }
 
-pub fn create_config(){
+pub fn create_config() -> Result<(), ParseError>{
     // we take the user inputted values and store it into a struct
     // we store the struct data into a text file
     // we break down the struct data into a readable format like json or something
@@ -18,6 +24,28 @@ pub fn create_config(){
             URL: "C:/Desktop/Test/test.html"
         );
     */
+    let address = "127.0.0.1:";
+    let mut port = String::new();
+    println!("Enter Port No::-> ");
+    let port_byte_size = std::io::stdin().read_line(&mut port).unwrap();
+
+    let mut path_url = String::new();
+    println!("Enter Page File Path::-> ");
+    let path_url_byte_size  = std::io::stdin().read_line(&mut path_url).unwrap();
+    
+    let full_addr = format!("{}{}",address,port);
+    let tcp = match full_addr.trim().parse::<String>(){
+        Ok(tcp) => tcp,
+        Err(e) => return Err(e)
+    };
+
+    
+    println!("[port: {},bytes_read: {}]",port,port_byte_size);
+    println!("[url: {},bytes_read: {}]",path_url,path_url_byte_size);
+    println!("[Full_address: {}]",full_addr);
+
+    
+    Ok(())
 }
     
 pub fn read_config(){
@@ -37,9 +65,37 @@ pub fn load_config(){
     // then when user opens the page at the specified port
 }    
     
-pub fn default_config(){
+pub fn default_config() -> i32{
     // load the default configuration
     // set by the program
     // port 3000
     // serves the hello.html page and 404.html page for errorinous condition
+    let tcp_listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+
+    for stream in tcp_listener.incoming(){
+        let _stream = stream.unwrap();
+
+        println!("Connection is Established!");
+        default_handle_connection(_stream);
+    }
+    let c= Config { port: 3000, url: "".to_string() };
+    return c.port;
+}
+
+fn default_handle_connection(mut connection_stream: TcpStream){
+    let buffered_reader = BufReader::new(&mut connection_stream);
+    let http_request = buffered_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = if http_request == "GET / HTTP/1.1" {
+            ("HTTP/1.1 200 OK", "hello.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
+
+        let contents = fs::read_to_string(filename).unwrap();
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        connection_stream.write_all(response.as_bytes()).unwrap();                                                                                                                            
 }
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,16 @@
-pub mod config;
+mod config;
 
+use ansi_term::*;
+use config::*;
 use std::{
     fs,
+    net::{TcpListener,TcpStream},
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
     string::ParseError,
 };
-fn main() -> Result<(), ParseError>{
-    let address = "127.0.0.1:";
-    let mut port = String::new();
-    println!("Enter Port No::-> ");
-    let port_byte_size = std::io::stdin().read_line(&mut port).unwrap();
 
-    let mut path_url = String::new();
-    println!("Enter Page File Path::-> ");
-    let path_url_byte_size  = std::io::stdin().read_line(&mut path_url).unwrap();
-    
-    let full_addr = format!("{}{}",address,port);
-    let tcp = match full_addr.trim().parse::<String>(){
-        Ok(tcp) => tcp,
-        Err(e) => return Err(e)
-    };
-
-    
-    println!("[port: {},bytes_read: {}]",port,port_byte_size);
-    println!("[url: {},bytes_read: {}]",path_url,path_url_byte_size);
-    println!("[Full_address: {}]",full_addr);
-
-    let tcp_listener = TcpListener::bind(tcp).unwrap();
-
-    for stream in tcp_listener.incoming(){
-        let _stream = stream.unwrap();
-
-        println!("Connection is Established!");
-        handle_connection(_stream);
-    }
-    Ok(())
-}
-
-fn handle_connection(mut connection_stream: TcpStream){
-    let buffered_reader = BufReader::new(&mut connection_stream);
-    let http_request = buffered_reader.lines().next().unwrap().unwrap();
-
-    let (status_line, filename) = if http_request == "GET / HTTP/1.1" {
-            ("HTTP/1.1 200 OK", "hello.html")
-        } else {
-            ("HTTP/1.1 404 NOT FOUND", "404.html")
-        };
-
-        let contents = fs::read_to_string(filename).unwrap();
-        let length = contents.len();
-
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-        connection_stream.write_all(response.as_bytes()).unwrap();                                                                                                                            
+fn main(){
+    //default_config();
+    let title = format!("{}",Style::new().bold().paint("WebServe"));
+    println!("{}",title);
 }
