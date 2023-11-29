7e7e2c6871fc595de3d2e7929d9956842301e9e4
IT21803420 <it21803420@my.sliit.lk>
2023-11-29 23:23:40 +0530

    implementing flags to executable file

--- exec/webserve.rs
+++ exec/webserve.rs
@@ -1,4 +1,42 @@
+use std::env;
+
+fn help(){
+    println!("LIST OF FLAGS")
+
+}
+
+
 
 fn main(){
-    print!("hallo");
+    let args: Vec<String> = env::args().collect();
+    if let Some(command) = args.get(1){
+        if command == "--test"{
+            println!("Test!");
+            for arg in &args{
+                println!("{}",arg);
+            }
+        }
+        else if command == "--hallo"{
+            println!("Hallo! {:?}",  &args[2..]);
+        } 
+        else if command == "--help"{
+            help();
+        }
+        else if command == "--create_config"{
+			println!("{:?}",c);
+        }
+        else if command == "--load_config"{
+
+        }
+        else if command == "--default_config"{
+
+        }
+        else if command == "--exit"{
+
+        }
+        else{
+            eprintln!("Command {command} doesnt exist!");
+        }
+        println!("Your argument {}", command);
+    }
 }
