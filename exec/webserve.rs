f5db34faea169bd45f4858c7fe61ec7522e05130
IT21803420 <it21803420@my.sliit.lk>
2023-12-04 21:05:59 +0530

    Implementing placeholders for the executable

--- exec/webserve.rs
+++ exec/webserve.rs
@@ -1,8 +1,13 @@
 use std::env;
 
 fn help(){
-    println!("LIST OF FLAGS")
-
+    println!("LIST OF FLAGS");
+    println!("--help\tDisplays List of Commands");
+    println!("--hallo <Secondary Arguments...>\tDisplays a Message to the User");
+    println!("--create_config\tCreates a new Network Configuration for the Server");
+    println!("--load_config\tLoads an existing Configuration for the Server");
+    println!("--default_config\tLoads the predefined Network Configuration for the Server");
+    println!("--exit\tExits the Application");
 }
 
 
@@ -23,16 +28,16 @@ fn main(){
             help();
         }
         else if command == "--create_config"{
-			println!("{:?}",c);
+			println!("{}","creating new configuration");
         }
         else if command == "--load_config"{
-
+            println!("{}","loading configuration options");
         }
         else if command == "--default_config"{
-
+            println!("{}","loading default configuration");
         }
         else if command == "--exit"{
-
+            println!("{}","exitting program");
         }
         else{
             eprintln!("Command {command} doesnt exist!");
